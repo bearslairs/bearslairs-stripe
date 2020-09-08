33b487dfed8c7a1143881379cd1b359a6fb678fb
Rob Thijssen <rthijssen@gmail.com>
2020-09-08 11:33:37 +0300

    stripe entity seeding

--- index.js
+++ index.js
@@ -3,42 +3,59 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_KEY);
 let seed = JSON.parse(fs.readFileSync('data/seed.json'));
-console.log(`discovered ${seed.products.length} seed products.`);
-stripe.products.list().then(x => x.data).then(stripeProducts => {
-  console.log(`discovered ${stripeProducts.length} stripe products.`);
-  seed.products.forEach(seedProduct => {
-    console.log(`- processing seed product: "${seedProduct.name}".`);
-    if (stripeProducts.some(x => x.name === seedProduct.name)) {
-      // seed product exists as a stripe product
-      let stripeProduct = stripeProducts.find(x => x.name === seedProduct.name);
-      console.log(`  - seed product: "${seedProduct.name}" discovered in stripe with id: "${stripeProduct.id}".`);
-      if (Object.keys(seedProduct).every(x => seedProduct[x] === stripeProduct[x])) {
-        // seed product matches stripe product
-        console.log(`  - all seed product properties match stripe product properties.`);
+
+let seedEntities = Object.keys(seed);
+console.log(`discovered ${seedEntities.length} seed entities.`);
+seedEntities.forEach(seedEntityPlural => {
+  let seedEntitySingular;
+  switch (seedEntityPlural) {
+    default:
+      seedEntitySingular = seedEntityPlural.slice(0, -1);
+      break;
+  }
+  console.log(`- processing seed entity: "${seedEntityPlural}".`);
+
+  console.log(`  - discovered ${seed[seedEntityPlural].length} seed ${seedEntityPlural}.`);
+
+  stripe[seedEntityPlural].list().then(x => x.data).then(stripeEntities => {
+    console.log(`  - discovered ${stripeEntities.length} stripe ${seedEntityPlural}.`);
+    seed[seedEntityPlural].forEach(seedEntity => {
+      console.log(`  - processing seed ${seedEntitySingular}: "${seedEntity.name}".`);
+      if (stripeEntities.some(x => x.name === seedEntity.name)) {
+        // seed entity exists as a stripe entity
+        let stripeEntity = stripeEntities.find(x => x.name === seedEntity.name);
+        console.log(`    - seed ${seedEntitySingular}: "${seedEntity.name}" discovered in stripe with id: "${stripeEntity.id}".`);
+        if (Object.keys(seedEntity).every(x => seedEntity[x] === stripeEntity[x])) {
+          // seed entity matches stripe entity
+          console.log(`    - all seed ${seedEntitySingular} properties match stripe ${seedEntitySingular} properties.`);
+        } else {
+          // seed entity differs from stripe entity
+          console.log(`    - one or more seed ${seedEntitySingular} properties differ from stripe ${seedEntitySingular} properties. update queued...`);
+          stripe[seedEntityPlural].update(
+            stripeEntity.id,
+            seedEntity
+          ).then(stripeEntityUpdateResponse => {
+            // stripe entity update succeeded
+            console.log(`stripe ${seedEntitySingular}: "${stripeEntityUpdateResponse.name}", with id: "${stripeEntityUpdateResponse.id}" updated.`);
+          }).catch(stripeEntityUpdateError => {
+            // stripe entity update failed
+            console.log(`stripe ${seedEntitySingular}: "${stripeEntity.name}", with id: "${stripeEntity.id}" update failed.`);
+            console.error(stripeEntityUpdateError);
+          });
+        }
       } else {
-        // seed product differs from stripe product
-        console.log(`  - one or more seed product properties differ from stripe product properties. update queued...`);
-        stripe.products.update(
-          stripeProduct.id,
-          seedProduct
-        ).then(stripeProductUpdateResponse => {
-          console.log(`  - stripe product: "${stripeProductUpdateResponse.name}", with id: "${stripeProductUpdateResponse.id}" updated.`);
-          //console.log(stripeProductUpdateResponse);
-        }).catch(stripeProductUpdateError => {
-          console.log(`  - stripe product: "${stripeProduct.name}", with id: "${stripeProduct.id}" update failed.`);
-          console.error(stripeProductUpdateError);
+        // seed entity does not exist as a stripe entity
+        console.log(`    - seed ${seedEntitySingular}: "${seedEntity.name}" not detected in stripe ${seedEntitySingular} list. create queued...`);
+        stripe[seedEntityPlural].create(seedEntity).then(stripeEntityCreateResponse => {
+          // stripe entity create succeeded
+          console.log(`stripe ${seedEntitySingular}: "${stripeEntityCreateResponse.name}", created with id: "${stripeEntityCreateResponse.id}".`);
+        }).catch(stripeEntityCreateError => {
+          // stripe entity create failed
+          console.log(`stripe ${seedEntitySingular}: "${stripeEntity.name}", create failed.`);
+          console.error(stripeEntityCreateError);
         });
       }
-    } else {
-      // seed product does not exist as a stripe product
-      console.log(`  - seed product: "${seedProduct.name}" not detected in stripe product list. create queued...`);
-      stripe.products.create(seedProduct).then(stripeProductCreateResponse => {
-        console.log(`  - stripe product: "${stripeProductCreateResponse.name}", created with id: "${stripeProductCreateResponse.id}".`);
-        //console.log(stripeProductCreateResponse);
-      }).catch(stripeProductCreateError => {
-        console.log(`  - stripe product: "${stripeProduct.name}", create failed.`);
-        console.error(stripeProductCreateError);
-      });
-    }
+    });
   });
 });
+
