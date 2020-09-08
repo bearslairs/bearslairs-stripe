b3860739095ca79435650451c1c1474c6d3f153b
Rob Thijssen <rthijssen@gmail.com>
2020-09-08 17:23:53 +0300

    stripe entiy mocking

--- index.js
+++ index.js
@@ -3,57 +3,96 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_KEY);
 let seed = JSON.parse(fs.readFileSync('data/seed.json'));
+let mock = JSON.parse(fs.readFileSync('data/mock.json'));
+let merged = { ...seed, ...mock };
 
-let seedEntities = Object.keys(seed);
-console.log(`discovered ${seedEntities.length} seed entities.`);
-seedEntities.forEach(seedEntityPlural => {
-  let seedEntitySingular;
-  switch (seedEntityPlural) {
+console.log(`discovered ${Object.keys(seed).length} seed entities.`);
+console.log(`discovered ${Object.keys(mock).length} mock entities.`);
+
+
+
+Object.keys(merged).forEach(entityPlural => {
+  let entitySingular, entityIdentifier;
+  switch (entityPlural) {
+    case 'customers':
+      entityIdentifier = 'email';
+      entitySingular = entityPlural.slice(0, -1);
+      break;
+    case 'prices':
+      entityIdentifier = 'nickname';
+      entitySingular = entityPlural.slice(0, -1);
+      break;
     default:
-      seedEntitySingular = seedEntityPlural.slice(0, -1);
+      entityIdentifier = 'name';
+      entitySingular = entityPlural.slice(0, -1);
       break;
   }
-  console.log(`- processing seed entity: "${seedEntityPlural}".`);
+  console.log(`- processing entity: "${entityPlural}".`);
 
-  console.log(`  - discovered ${seed[seedEntityPlural].length} seed ${seedEntityPlural}.`);
+  console.log(`  - discovered ${merged[entityPlural].length} ${entityPlural}.`);
 
-  stripe[seedEntityPlural].list().then(x => x.data).then(stripeEntities => {
-    console.log(`  - discovered ${stripeEntities.length} stripe ${seedEntityPlural}.`);
-    seed[seedEntityPlural].forEach(seedEntity => {
-      console.log(`  - processing seed ${seedEntitySingular}: "${seedEntity.name}".`);
-      if (stripeEntities.some(x => x.name === seedEntity.name)) {
-        // seed entity exists as a stripe entity
-        let stripeEntity = stripeEntities.find(x => x.name === seedEntity.name);
-        console.log(`    - seed ${seedEntitySingular}: "${seedEntity.name}" discovered in stripe with id: "${stripeEntity.id}".`);
-        if (Object.keys(seedEntity).every(x => seedEntity[x] === stripeEntity[x])) {
-          // seed entity matches stripe entity
-          console.log(`    - all seed ${seedEntitySingular} properties match stripe ${seedEntitySingular} properties.`);
+  stripe[entityPlural].list({ limit: 100 }).then(x => x.data).then(stripeEntities => {
+    console.log(`  - discovered ${stripeEntities.length} stripe ${entityPlural}.`);
+    merged[entityPlural].forEach(entity => {
+      console.log(`  - processing ${entitySingular}: "${entity[entityIdentifier]}".`);
+      if (stripeEntities.some(x => x[entityIdentifier] === entity[entityIdentifier])) {
+        // seed/mock entity exists as a stripe entity
+        let stripeEntity = stripeEntities.find(x => x[entityIdentifier] === entity[entityIdentifier]);
+        console.log(`    - ${entitySingular}: "${entity[entityIdentifier]}" discovered in stripe with id: "${stripeEntity.id}".`);
+        if (Object.keys(entity).every(x => (((typeof entity[x] !== 'boolean') && (typeof entity[x] !== 'number') && (typeof entity[x] !== 'string')) || (entity[x] === stripeEntity[x])))) {
+          // seed/mock entity matches stripe entity
+          console.log(`    - all ${entitySingular} properties match stripe ${entitySingular} properties.`);
         } else {
-          // seed entity differs from stripe entity
-          console.log(`    - one or more seed ${seedEntitySingular} properties differ from stripe ${seedEntitySingular} properties. update queued...`);
-          stripe[seedEntityPlural].update(
+          // seed/mock entity differs from stripe entity
+          console.log(`    - one or more ${entitySingular} properties differ from stripe ${entitySingular} properties. update queued...`);
+          switch (entitySingular) {
+            case 'price':
+              // price properties for which update is not supported
+              delete entity.currency;
+              delete entity.unit_amount;
+              delete entity.recurring;
+              break;
+          }
+          stripe[entityPlural].update(
             stripeEntity.id,
-            seedEntity
+            entity
           ).then(stripeEntityUpdateResponse => {
             // stripe entity update succeeded
-            console.log(`stripe ${seedEntitySingular}: "${stripeEntityUpdateResponse.name}", with id: "${stripeEntityUpdateResponse.id}" updated.`);
+            console.log(`stripe ${entitySingular}: "${stripeEntityUpdateResponse[entityIdentifier]}", with id: "${stripeEntityUpdateResponse.id}" updated.`);
           }).catch(stripeEntityUpdateError => {
             // stripe entity update failed
-            console.log(`stripe ${seedEntitySingular}: "${stripeEntity.name}", with id: "${stripeEntity.id}" update failed.`);
+            console.log(`stripe ${entitySingular}: "${stripeEntity[entityIdentifier]}", with id: "${stripeEntity.id}" update failed.`);
             console.error(stripeEntityUpdateError);
           });
         }
       } else {
-        // seed entity does not exist as a stripe entity
-        console.log(`    - seed ${seedEntitySingular}: "${seedEntity.name}" not detected in stripe ${seedEntitySingular} list. create queued...`);
-        stripe[seedEntityPlural].create(seedEntity).then(stripeEntityCreateResponse => {
-          // stripe entity create succeeded
-          console.log(`stripe ${seedEntitySingular}: "${stripeEntityCreateResponse.name}", created with id: "${stripeEntityCreateResponse.id}".`);
-        }).catch(stripeEntityCreateError => {
-          // stripe entity create failed
-          console.log(`stripe ${seedEntitySingular}: "${stripeEntity.name}", create failed.`);
-          console.error(stripeEntityCreateError);
-        });
+        // seed/mock entity does not exist as a stripe entity
+        console.log(`    - ${entitySingular}: "${entity[entityIdentifier]}" not detected in stripe ${entitySingular} list. create queued...`);
+        switch (entitySingular) {
+          case 'price':
+            stripe.products.list({ limit: 100 }).then(x => x.data).then(products => {
+              entity.product = products.find(x => x.name === entity[entityIdentifier].replace(/^(standard|corona)\s/i, '')).id;
+              stripe[entityPlural].create(entity).then(stripeEntityCreateResponse => {
+                // stripe entity create succeeded
+                console.log(`stripe ${entitySingular}: "${stripeEntityCreateResponse[entityIdentifier]}", created with id: "${stripeEntityCreateResponse.id}".`);
+              }).catch(stripeEntityCreateError => {
+                // stripe entity create failed
+                console.log(`stripe ${entitySingular}: "${entity[entityIdentifier]}", create failed.`);
+                console.error(stripeEntityCreateError);
+              });
+            });
+            break;
+          default:
+            stripe[entityPlural].create(entity).then(stripeEntityCreateResponse => {
+              // stripe entity create succeeded
+              console.log(`stripe ${entitySingular}: "${stripeEntityCreateResponse[entityIdentifier]}", created with id: "${stripeEntityCreateResponse.id}".`);
+            }).catch(stripeEntityCreateError => {
+              // stripe entity create failed
+              console.log(`stripe ${entitySingular}: "${entity[entityIdentifier]}", create failed.`);
+              console.error(stripeEntityCreateError);
+            });
+            break;
+        }
       }
     });
   });
